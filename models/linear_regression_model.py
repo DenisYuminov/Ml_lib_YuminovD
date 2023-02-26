681d4b18638cf5452c4fbfa3b0e9ad043c566f46
DenisYuminov <97443918+DenisYuminov@users.noreply.github.com>
2023-02-26 23:39:37 +0300

    hw1 done

--- models/linear_regression_model.py
+++ models/linear_regression_model.py
@@ -1,9 +1,10 @@
 import numpy as np
 
+
 class LinearRegression():
 
     def __init__(self, base_functions: list):
-        self.weights = None # TODO: init weights using np.random.randn (normal distribution with mean=0 and variance=1)
+        self.weights = np.random.randn(1, len(base_functions))
         self.base_functions = base_functions
 
     @staticmethod
@@ -12,16 +13,17 @@ class LinearRegression():
         pass
 
     def __plan_matrix(self, inputs: np.ndarray) -> np.ndarray:
-        # TODO build Plan matrix using list of lambda functions defined in config. Use only one loop (for base_functions)
-        pass
+        plan_matrix = []
+        for base_function in self.base_functions:
+            plan_matrix.append(base_function(inputs))
+        return np.array(plan_matrix)
 
     def __calculate_weights(self, pseudoinverse_plan_matrix: np.ndarray, targets: np.ndarray) -> None:
         """calculate weights of the model using formula from the lecture. Not this homework"""
         pass
 
     def calculate_model_prediction(self, plan_matrix) -> np.ndarray:
-        # TODO calculate prediction of the model (y) using formula from the lecture
-        pass
+        return self.weights.dot(plan_matrix)
 
     def train_model(self, inputs: np.ndarray, targets: np.ndarray) -> None:
         """Not this homework"""
--- utils/visualisation.py
+++ utils/visualisation.py
@@ -5,7 +5,7 @@ class Visualisation():
 
     @staticmethod
     def visualise_predicted_trace(prediction:np.ndarray,inputs:np.ndarray,targets:np.ndarray,plot_title=''):
-        # TODO visualise predicted trace and targets
+        #  visualise predicted trace and targets
         """
 
         :param prediction: model prediction based on inputs (oy for one trace)
@@ -13,7 +13,27 @@ class Visualisation():
         :param targets: target variables (oy for one trace)
         :param plot_title: plot title
         """
-        pass
+        figure = go.Figure()
+
+        targets_array = go.Scatter(
+            x=inputs,
+            y=targets,
+            name="Target",
+            mode='markers'
+        )
+
+        prediction_array = go.Scatter(
+            x=inputs,
+            y=prediction.flatten(),
+            name="Prediction",
+            mode='lines'
+        )
+        figure.update_layout(
+            title=plot_title,
+            legend_title="Legend type"
+        )
+        figure.add_traces([targets_array, prediction_array])
+        figure.show()
 
     @staticmethod
     def visualise_error():
